6fc41585938f01ac9789256d0c384ef6598e115c
sjava <sjava@163.com>
2014-11-24 22:17:21 +0800

    ipython auto complete

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,6 +1,17 @@
 (require 'python)
-(setq python-shell-interpreter "ipython")
-(setq python-shell-interpreter-args "--pylab")
+
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--pylab"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
